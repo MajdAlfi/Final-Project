1e0fff832dfe236b8a28f8c1d87b20f9e7126ef3
Softwaresan <sansamir2001@yahoo.com>
2023-04-08 02:19:39 +0300

    design signup screen

--- final_project/lib/main.dart
+++ final_project/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:final_project/Src/Screens/firstUi.dart';
 import 'package:final_project/Src/Screens/homeScr.dart';
 import 'package:final_project/Src/Screens/profileScr.dart';
 import 'package:final_project/Src/Screens/projectsScr.dart';
+import 'package:final_project/Src/Screens/signUp.dart';
 import 'package:final_project/Src/Services/dataprovider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
